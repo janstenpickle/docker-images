ca6cf0a4e6c3e3a5e5d6d48b6296b64ad72c155f
janstenpickle <jansen.chris@gmail.com>
2013-10-12 20:48:11 +0000

    update mariadb

--- mariadb/start_mariadb.sh
+++ mariadb/start_mariadb.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-ROOT_PASSWORD=$MARIA_ROOT_PASS
-
 if [ ! -d /data/mysql ]; then
   mkdir /data/mysql
 fi
 
 if [ "x$(ls /data/mysql | wc -l)" == "x0" ]; then
   mysql_install_db
-  su - mysql -s /bin/bash -c "/usr/bin/mysqladmin -u root password $ROOT_PASSWORD"
 fi
 
 su - mysql -s /bin/bash -c mysqld_safe

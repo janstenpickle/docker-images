027382baec48cfd6a18bf5469dd5e06a11e7e08d
janstenpickle <jansen.chris@gmail.com>
2013-10-13 13:09:22 +0000

    startup and config finalisation

--- mariadb/start_mariadb.sh
+++ mariadb/start_mariadb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cat /proc/mounts > /etc/mtab
+
 if [ ! -d /data/mysql ]; then
   mkdir /data/mysql
 fi
@@ -8,4 +10,12 @@ if [ "x$(ls /data/mysql | wc -l)" == "x0" ]; then
   mysql_install_db
 fi
 
-su - mysql -s /bin/bash -c mysqld_safe
+su - mysql -s /bin/bash -c mysqld_safe &
+
+sleep 3
+
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.17.%.%' WITH GRANT OPTION;" | mysql
+
+while true; do
+  sleep 1000
+done

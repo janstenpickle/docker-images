5c01a6fbcf90efd764da98a99f0cb95fb7382083
janstenpickle <jansen.chris@gmail.com>
2013-10-14 20:26:26 +0000

    upload dire permissions

--- wordpress/start_wp.sh
+++ wordpress/start_wp.sh
@@ -2,6 +2,12 @@
 
 puppet apply /etc/puppet/manifests/wpconf.pp
 
+if [ ! -d /data/uploads ]; then 
+  mkdir -p /data/uploads
+fi
+chown www-data:www-data /data/uploads
+ln -s /data/uploads /wordpress/wp-content/uploads
+
 php5-fpm &
 nginx
 
